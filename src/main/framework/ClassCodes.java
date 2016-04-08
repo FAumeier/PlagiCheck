b9ecf8648c6d6f1ea33fe7b3c5ad71c391b01b35
Matthias Strobl <strobl1@hm.edu>
2016-04-08 14:09:46 +0200

    Added enumeration for different class codes (to be used in IToken)

---
+++ src/main/framework/ClassCodes.java
@@ -0,0 +1,34 @@
+package framework;
+
+/**
+ * Class Codes for Tokenizer
+ */
+public enum ClassCodes {
+    IDENTIFIER(0, "Identifier"),
+    DATE(1, "Date"),
+    WS(2, "Whitespace"),
+    PMARK(3, "Punktuationmark"),
+    EOF(4, "End of File");
+
+    private final int id;
+    private final String description;
+
+
+    ClassCodes(int id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+}
