2aaa66961fd4a19a698e6e6e8a912e9e1506f727
Florian Aumeier <aumeier@protonmail.com>
2016-04-08 13:57:28 +0200

    Added -std Option to PlagiCheck -std allows the user to pass to strings: "original string" "suspect string"

--- src/main/framework/PlagiCheck.java
+++ src/main/framework/PlagiCheck.java
@@ -1,6 +1,8 @@
 package framework;
 
 
+import java.io.PrintWriter;
+
 /**
  * Created by Matthias on 18.03.2016.
  */
@@ -9,8 +11,20 @@ public class PlagiCheck {
         if (args.length == 2) {
             AlignmentController controller = new AlignmentController(args[0], args[1]);
             controller.run();
-        }
-        else {
+        } else if (args.length == 3) {
+            if (args[0].equalsIgnoreCase("-std")) {
+                try(  PrintWriter out = new PrintWriter( "original.txt" )  ){
+                    out.println( args[1] );
+                }
+                try(  PrintWriter out = new PrintWriter( "suspect.txt" )  ){
+                    out.println( args[2] );
+                }
+                AlignmentController controller = new AlignmentController("original.txt", "suspect.txt");
+                controller.run();
+            } else {
+                throw new Exception("Only avialable Option is -std input1 input2");
+            }
+        } else {
             throw new Exception("Wir erwarten den Namen des Originals und den Namen des Plagiats");
         }
     }
