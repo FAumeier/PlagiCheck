d646740b4c8ef6d2b786ddec3bb99992e1217c89
Florian Aumeier <aumeier@protonmail.com>
2016-05-07 16:45:42 +0200

    removed assertToken helper method in BaseLexerTest. Adapted the test cases

--- src/test/framework/BaseLexerTest.java
+++ src/test/framework/BaseLexerTest.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.PushbackReader;
 import java.io.StringReader;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Tests the base lexer
@@ -20,12 +20,6 @@ public class BaseLexerTest {
     public void setUp() throws Exception {
         sentence = "Bla dies ist ein, Satz geschrieben am 12.10.16";
         lexer = new BaseLexer(new PushbackReader(new StringReader(sentence)));
-    }
-
-    /** Helper to assert equality of class and relative code for a token */
-    private void assertToken(IToken token, ClassCodes classCode, int relativeCode) {
-        assertThat(token.getClassCode()).isEqualTo(classCode);
-        assertThat(token.getRelativeCode()).isEqualTo(relativeCode);
     }
 
     @Test
@@ -64,12 +58,12 @@ public class BaseLexerTest {
     public void shouldTokenizeTestSentence() {
         ILexer sut = new BaseLexer(new PushbackReader(new StringReader("Hallo 242  Test "), 4));
         try {
-            assertToken(sut.getNextToken(), ClassCodes.IDENTIFIER, 0);
-            assertToken(sut.getNextToken(), ClassCodes.WS, 0);
-            assertToken(sut.getNextToken(), ClassCodes.INTCONS, 0);
-            assertToken(sut.getNextToken(), ClassCodes.WS, 1);
-            assertToken(sut.getNextToken(), ClassCodes.IDENTIFIER, 1);
-            assertToken(sut.getNextToken(), ClassCodes.WS, 0);
+            assertThat(sut.getNextToken().equals(new Token(ClassCodes.IDENTIFIER, 0)));
+            assertThat(sut.getNextToken().equals(new Token(ClassCodes.WS, 0)));
+            assertThat(sut.getNextToken().equals(new Token(ClassCodes.INTCONS, 0)));
+            assertThat(sut.getNextToken().equals(new Token(ClassCodes.WS, 1)));
+            assertThat(sut.getNextToken().equals(new Token(ClassCodes.IDENTIFIER, 1)));
+            assertThat(sut.getNextToken().equals(new Token(ClassCodes.WS, 0)));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -81,7 +75,7 @@ public class BaseLexerTest {
         try {
             for (int i = 0; i < dates.length; i++) {
                 ILexer sut = new BaseLexer(new PushbackReader(new StringReader(dates[i]), 4));
-                assertToken(sut.getNextToken(), ClassCodes.DATE, 0);
+                assertThat(sut.getNextToken().equals(new Token(ClassCodes.DATE, 0)));
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -92,14 +86,14 @@ public class BaseLexerTest {
     public void shouldDistiguishIntconsFromDate() {
         ILexer sut = new BaseLexer(new PushbackReader(new StringReader("02.04 nodate 02."), 4));
         try {
-            assertToken(sut.getNextToken(), ClassCodes.INTCONS, 0);
-            assertToken(sut.getNextToken(), ClassCodes.PMARK, 0);
-            assertToken(sut.getNextToken(), ClassCodes.INTCONS, 1);
-            assertToken(sut.getNextToken(), ClassCodes.WS, 0);
-            assertToken(sut.getNextToken(), ClassCodes.IDENTIFIER, 0);
-            assertToken(sut.getNextToken(), ClassCodes.WS, 0);
-            assertToken(sut.getNextToken(), ClassCodes.INTCONS, 0);
-            assertToken(sut.getNextToken(), ClassCodes.PMARK, 0);
+            assertThat(sut.getNextToken().equals(new Token(ClassCodes.INTCONS, 0)));
+            assertThat(sut.getNextToken().equals(new Token(ClassCodes.PMARK, 0)));
+            assertThat(sut.getNextToken().equals(new Token(ClassCodes.INTCONS, 1)));
+            assertThat(sut.getNextToken().equals(new Token(ClassCodes.WS, 0)));
+            assertThat(sut.getNextToken().equals(new Token(ClassCodes.IDENTIFIER, 0)));
+            assertThat(sut.getNextToken().equals(new Token(ClassCodes.WS, 0)));
+            assertThat(sut.getNextToken().equals(new Token(ClassCodes.INTCONS, 0)));
+            assertThat(sut.getNextToken().equals(new Token(ClassCodes.PMARK, 0)));
         } catch (IOException e) {
             e.printStackTrace();
         }
